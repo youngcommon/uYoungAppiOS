52c5368bfad57510fdf901fe9d8f648a23f308e8
zhaoquan <youngcommon@gmail.com>
2015-09-23 21:35:07 +0800

    增加活动model

---
+++ uYoung/uYoung/ActivityModel.m
@@ -0,0 +1,13 @@
+//
+//  ActivityModel.m
+//  uYoung
+//
+//  Created by CSDN on 15/9/23.
+//  Copyright © 2015年 uYoungCommon. All rights reserved.
+//
+
+#import "ActivityModel.h"
+
+@implementation ActivityModel
+
+@end
