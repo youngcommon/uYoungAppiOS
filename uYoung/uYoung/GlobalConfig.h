afcf79f994dbf956bd0bf57c4ac9e2ede005fba1
zhaoquan <youngcommon@gmail.com>
2015-09-23 16:43:19 +0800

    增加1倍图标

---
+++ uYoung/uYoung/GlobalConfig.h
@@ -0,0 +1,13 @@
+//
+//  GlobalConfig.h
+//  uYoung
+//
+//  Created by CSDN on 15/9/23.
+//  Copyright © 2015年 uYoungCommon. All rights reserved.
+//
+
+#ifndef GlobalConfig_h
+#define GlobalConfig_h
+
+
+#endif /* GlobalConfig_h */
