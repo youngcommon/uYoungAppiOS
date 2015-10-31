a4eb8e6f527847c0ce8fbb3c56945f54cd47ad7c
zhaoquan <youngcommon@gmail.com>
2015-10-31 22:49:50 +0800

    修改活动详情，根据屏幕尺寸自动适配

--- uYoung/uYoung/ActivityDetailViewController.h
+++ uYoung/uYoung/ActivityDetailViewController.h
@@ -22,63 +22,30 @@
 @property (strong, nonatomic) ActivityDetailModel *detailModel;
 @property (strong, nonatomic) UYoungUser *loginUser;
 
+@property (weak, nonatomic) IBOutlet UIView *contentView;
+
 @property (weak, nonatomic) IBOutlet UIImageView *headerBackground;
 @property (weak, nonatomic) IBOutlet UILabel *titleLable;
 @property (weak, nonatomic) IBOutlet UIButton *backBtn;
 @property (weak, nonatomic) IBOutlet UIButton *userHeader;
 
-@property (weak, nonatomic) IBOutlet UILabel *actDate;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actDateConsHeight;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actDateConsWidth;
-
-@property (weak, nonatomic) IBOutlet UILabel *actDateTitle;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actDateTitleConsWidth;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actDateTitleConsHeight;
-
-@property (weak, nonatomic) IBOutlet UILabel *actTime;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actTimeConsHeight;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actTImeConsWidth;
-
-@property (weak, nonatomic) IBOutlet UILabel *actTimeTitle;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actTimeTitleConsWidth;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actTimeTitleConsHeight;
-
-@property (weak, nonatomic) IBOutlet UILabel *actType;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actTypeConsWidth;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actTypeConsHeight;
-
-@property (weak, nonatomic) IBOutlet UILabel *actTypeTitle;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actTypeTitleConsWidth;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *actTypeTitleConsHeight;
-
-@property (weak, nonatomic) IBOutlet UILabel *enrollPersons;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *enrollPersonsConsWidht;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *enrollPersonsConsHeight;
-
-@property (weak, nonatomic) IBOutlet UILabel *enrollPersonsTitle;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *enrollPersonsTitleConsHeight;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *enrollPersonsTitleConsWidth;
-
-@property (weak, nonatomic) IBOutlet UILabel *organizer;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *orgConsHeight;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *orgConsWidth;
-
-@property (weak, nonatomic) IBOutlet UILabel *organizerTitle;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *orgConsTitleWidth;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *orgConsTitleHeight;
-
-@property (weak, nonatomic) IBOutlet UILabel *addr;
-
-@property (weak, nonatomic) IBOutlet UILabel *addrTitle;
-
-
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *sepLineCons;
+@property (strong, nonatomic) UILabel *actDate;
+@property (strong, nonatomic) UILabel *actDateTitle;
+@property (strong, nonatomic) UILabel *actTime;
+@property (strong, nonatomic) UILabel *actTimeTitle;
+@property (strong, nonatomic) UILabel *actType;
+@property (strong, nonatomic) UILabel *actTypeTitle;
+@property (strong, nonatomic) UILabel *enrollPersons;
+@property (strong, nonatomic) UILabel *enrollPersonsTitle;
+@property (strong, nonatomic) UILabel *organizer;
+@property (strong, nonatomic) UILabel *organizerTitle;
+@property (strong, nonatomic) UILabel *addr;
+@property (strong, nonatomic) UILabel *addrTitle;
 
 @property (weak, nonatomic) IBOutlet UIImageView *freeSignetImg;
 @property (weak, nonatomic) IBOutlet UIScrollView *descScrollView;
 @property (weak, nonatomic) IBOutlet UIButton *button;
-
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *addrHeight;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *addrTitleHeight;
 
 @property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
 
--- uYoung/uYoung/ActivityDetailViewController.m
+++ uYoung/uYoung/ActivityDetailViewController.m
@@ -32,24 +32,145 @@ static NSString * const reuseIdentifier = @"Cell";
     self.userHeader.layer.masksToBounds = YES;
     
     self.titleLable.text = self.model.title;
-    self.actType.text = [NSString stringWithFormat:@"%@摄影", self.model.actType];
-    
-    self.actDate.text = [NSString stringWithFormat:@"%d月%d日", self.model.month, self.model.day];
-    self.actTime.text = [NSString stringWithFormat:@"%@-%@", self.model.fromTime, self.model.toTime];
-    self.addr.text = self.model.addr;
     if(self.model.price==0){
         [self.freeSignetImg setHidden:YES];
     }
     
-    //========================活动地址约束调整=========================
-    CGFloat addrTitleWidth = self.addrTitle.frame.size.width;
-    CGSize addrTitleLabelSize = [self.addrTitle sizeThatFits:CGSizeMake(addrTitleWidth, MAXFLOAT)];
-    [self.addrTitleHeight setConstant:addrTitleLabelSize.height];
+    CGFloat x = 20;
+    CGFloat y = 30;
+    //根据屏幕宽度，增加label字号，6增一，plus增二
+    NSString *fontName = [self.actDate.font fontName];
+    UIFont *font;
 
-    CGFloat addrWidth = mScreenWidth - 22 * 2 - 15 * 2 - 5 - addrTitleWidth;
-    CGSize addrLabelSize = [self.addr sizeThatFits:CGSizeMake(addrWidth, MAXFLOAT)];
-    [self.addrHeight setConstant:addrLabelSize.height];
-    //================================================
+    CGFloat fontSize;
+    if (mScreenWidth==375) {//iPhone 6
+        font = [UIFont fontWithName:fontName size:14];
+        fontSize = 14.f;
+    }else if(mScreenWidth>375){//iPhone 6+
+        font = [UIFont fontWithName:fontName size:16];
+        fontSize = 16.f;
+    }else{
+        font = [UIFont fontWithName:fontName size:11];
+        fontSize = 11.f;
+    }
+        
+    CGSize actDateTitleSize = [@"活动日期:" sizeWithFont:[UIFont systemFontOfSize:fontSize] constrainedToSize:CGSizeMake(mScreenWidth/2, MAXFLOAT) lineBreakMode:NSLineBreakByClipping];
+    _actDateTitle = [[UILabel alloc]initWithFrame:CGRectMake(x, y, actDateTitleSize.width, actDateTitleSize.height)];
+    [_actDateTitle setText:@"活动日期:"];
+    [_actDateTitle setFont:font];
+    [_actDateTitle setTextColor:UIColorFromRGB(0x4a90e2)];
+    [_actDateTitle setLineBreakMode:NSLineBreakByClipping];
+    [_contentView addSubview:_actDateTitle];
+    
+    CGFloat width = actDateTitleSize.width + 5;
+        
+    x = x + actDateTitleSize.width + 5;
+    NSString *actDateStr = [NSString stringWithFormat:@"%d月%d日", (int)self.model.month, (int)self.model.day];
+    CGSize actDateSize = [actDateStr sizeWithFont:[UIFont systemFontOfSize:fontSize] constrainedToSize:CGSizeMake(width, MAXFLOAT) lineBreakMode:NSLineBreakByClipping];
+    _actDate = [[UILabel alloc]initWithFrame:CGRectMake(x, y, actDateSize.width, actDateSize.height)];
+    [_actDate setText:actDateStr];
+    [_actDate setFont:font];
+    [_actDate setTextColor:UIColorFromRGB(0x85b200)];
+    [_actDate setLineBreakMode:NSLineBreakByClipping];
+    [_contentView addSubview:_actDate];
+        
+    x = x + actDateSize.width + 10;
+    _actTimeTitle = [[UILabel alloc]initWithFrame:CGRectMake(x, y, actDateTitleSize.width, actDateTitleSize.height)];
+    [_actTimeTitle setText:@"活动时长:"];
+    [_actTimeTitle setFont:font];
+    [_actTimeTitle setTextColor:UIColorFromRGB(0x4a90e2)];
+    [_actTimeTitle setLineBreakMode:NSLineBreakByClipping];
+    [_contentView addSubview:_actTimeTitle];
+    
+    CGFloat actTimeSizeLeft = mScreenWidth - 22*2 - 15*2 - actDateTitleSize.width - 5 - (actDateSize.width*2) - 10;
+    x = x + actDateTitleSize.width + 5;
+    NSString *actTimeStr = [NSString stringWithFormat:@"%@-%@", self.model.fromTime, self.model.toTime];
+    CGSize actTimeSize = [actTimeStr sizeWithFont:[UIFont systemFontOfSize:fontSize] constrainedToSize:CGSizeMake(actTimeSizeLeft, MAXFLOAT) lineBreakMode:NSLineBreakByClipping];
+    _actTime = [[UILabel alloc]initWithFrame:CGRectMake(x, y, actTimeSize.width, actTimeSize.height)];
+    [_actTime setText:actTimeStr];
+    [_actTime setFont:font];
+    [_actTime setTextColor:UIColorFromRGB(0x85b200)];
+    [_actTime setLineBreakMode:NSLineBreakByClipping];
+    [_contentView addSubview:_actTime];
+        
+    //第二行标签
+    y = y + actDateTitleSize.height + 10;
+    x = 20;
+    _actTypeTitle = [[UILabel alloc]initWithFrame:CGRectMake(x, y, actDateTitleSize.width, actDateTitleSize.height)];
+    [_actTypeTitle setText:@"活动类型:"];
+    [_actTypeTitle setFont:font];
+    [_actTypeTitle setTextColor:UIColorFromRGB(0x4a90e2)];
+    [_actTypeTitle setLineBreakMode:NSLineBreakByClipping];
+    [_contentView addSubview:_actTypeTitle];
+        
+    x = x + actDateTitleSize.width + 5;
+    NSString *actTypeStr = [NSString stringWithFormat:@"%@摄影", self.model.actType];
+    CGSize actTypeSize = [actTypeStr sizeWithFont:[UIFont systemFontOfSize:fontSize] constrainedToSize:CGSizeMake(width, MAXFLOAT) lineBreakMode:NSLineBreakByClipping];
+    _actType = [[UILabel alloc]initWithFrame:CGRectMake(x, y, actTypeSize.width, actTypeSize.height)];
+    [_actType setText:actTypeStr];
+    [_actType setFont:font];
+    [_actType setTextColor:UIColorFromRGB(0x85b200)];
+    [_actType setLineBreakMode:NSLineBreakByClipping];
+    [_contentView addSubview:_actType];
+        
+    x = _actTimeTitle.frame.origin.x;
+    _enrollPersonsTitle = [[UILabel alloc]initWithFrame:CGRectMake(x, y, actDateTitleSize.width, actDateTitleSize.height)];
+    [_enrollPersonsTitle setText:@"参与人数:"];
+    [_enrollPersonsTitle setFont:font];
+    [_enrollPersonsTitle setTextColor:UIColorFromRGB(0x4a90e2)];
+    [_enrollPersonsTitle setLineBreakMode:NSLineBreakByClipping];
+    [_contentView addSubview:_enrollPersonsTitle];
+    
+    x = x + actDateTitleSize.width + 5;
+    _enrollPersons = [[UILabel alloc]initWithFrame:CGRectMake(x, y, actTimeSize.width, actTimeSize.height)];
+    [_enrollPersons setText:@"-- / --"];
+    [_enrollPersons setFont:font];
+    [_enrollPersons setTextColor:UIColorFromRGB(0x85b200)];
+    [_enrollPersons setLineBreakMode:NSLineBreakByClipping];
+    [_contentView addSubview:_enrollPersons];
+    
+    //第三行标签
+    x = 20;
+    y = y + actDateTitleSize.height + 10;
+    _organizerTitle = [[UILabel alloc]initWithFrame:CGRectMake(x, y, actDateTitleSize.width, actDateTitleSize.height)];
+    [_organizerTitle setText:@"组织者:"];
+    [_organizerTitle setFont:font];
+    [_organizerTitle setTextColor:UIColorFromRGB(0x4a90e2)];
+    [_organizerTitle setLineBreakMode:NSLineBreakByClipping];
+    [_contentView addSubview:_organizerTitle];
+        
+    width = mScreenWidth - 22*2 - 15*2 - 5 - actDateTitleSize.width;
+    x = _actType.frame.origin.x;
+    NSString *orgStr = @"abcabcabcabcabcabc";
+    CGSize orgStrSize = [orgStr sizeWithFont:[UIFont systemFontOfSize:fontSize] constrainedToSize:CGSizeMake(width, MAXFLOAT) lineBreakMode:NSLineBreakByClipping];
+    _organizer = [[UILabel alloc]initWithFrame:CGRectMake(x, y, orgStrSize.width, orgStrSize.height)];
+    [_organizer setText:orgStr];
+    [_organizer setFont:font];
+    [_organizer setTextColor:UIColorFromRGB(0x85b200)];
+    [_organizer setLineBreakMode:NSLineBreakByClipping];
+    [_contentView addSubview:_organizer];
+        
+    //第四行标签
+    x = 20;
+    y = y + actDateTitleSize.height + 10;
+    _addrTitle = [[UILabel alloc]initWithFrame:CGRectMake(x, y, actDateTitleSize.width, actDateTitleSize.height)];
+    [_addrTitle setText:@"活动地点:"];
+    [_addrTitle setFont:font];
+    [_addrTitle setTextColor:UIColorFromRGB(0x4a90e2)];
+    [_addrTitle setLineBreakMode:NSLineBreakByClipping];
+    [_contentView addSubview:_addrTitle];
+        
+    x = _organizer.frame.origin.x;
+    NSString *addrStr = self.model.addr;
+    CGSize addrStrSize = [addrStr sizeWithFont:[UIFont systemFontOfSize:fontSize] constrainedToSize:CGSizeMake(width, MAXFLOAT) lineBreakMode:NSLineBreakByClipping];
+    _addr = [[UILabel alloc]initWithFrame:CGRectMake(x, y, addrStrSize.width, addrStrSize.height)];
+    [_addr setText:addrStr];
+    [_addr setFont:font];
+    [_addr setTextColor:UIColorFromRGB(0x85b200)];
+    [_addr setLineBreakMode:NSLineBreakByClipping];
+    [_addr setNumberOfLines:0];
+    [_contentView addSubview:_addr];
+    [_sepLineCons setConstant:(y+_addr.frame.size.height+15)];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fillActivityDetail:) name:@"fillActivityDetail" object:nil];
     
@@ -82,7 +203,7 @@ static NSString * const reuseIdentifier = @"Cell";
     NSDictionary *dic = (NSDictionary*)[notification object];
     self.detailModel = [MTLJSONAdapter modelOfClass:[ActivityDetailModel class] fromJSONDictionary:dic error:nil];
     
-    self.enrollPersons.text = [NSString stringWithFormat:@"%d / %d", self.detailModel.realNum, self.detailModel.needNum];
+    self.enrollPersons.text = [NSString stringWithFormat:@"%d / %d", (int)self.detailModel.realNum, (int)self.detailModel.needNum];
     self.organizer.text = self.detailModel.nickName;
     NSString *desc = self.detailModel.desc;
     
