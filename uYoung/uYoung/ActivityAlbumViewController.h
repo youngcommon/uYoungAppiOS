727b051a1fcdd76dbe6565c824db23623a209962
独自天涯 <duzitianya@hotmail.com>
2015-11-26 23:38:33 +0800

    创建活动相册页面

---
+++ uYoung/uYoung/ActivityAlbumViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActivityAlbumViewController.h
+//  uYoung
+//
+//  Created by 独自天涯 on 15/11/26.
+//  Copyright © 2015年 uYoungCommon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityAlbumViewController : UIViewController
+
+@end
---
+++ uYoung/uYoung/ActivityAlbumViewController.m
@@ -0,0 +1,27 @@
+//
+//  ActivityAlbumViewController.m
+//  uYoung
+//
+//  Created by 独自天涯 on 15/11/26.
+//  Copyright © 2015年 uYoungCommon. All rights reserved.
+//
+
+#import "ActivityAlbumViewController.h"
+
+@interface ActivityAlbumViewController ()
+
+@end
+
+@implementation ActivityAlbumViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
