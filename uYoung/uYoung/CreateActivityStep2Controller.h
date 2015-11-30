37d1d6155871c71f72c5aba3297c6f74b71c779a
独自天涯 <duzitianya@hotmail.com>
2015-11-30 21:11:46 +0800

    发布活动第二步

---
+++ uYoung/uYoung/CreateActivityStep2Controller.h
@@ -0,0 +1,13 @@
+//
+//  CreateActivityStep2Controller.h
+//  uYoung
+//
+//  Created by 独自天涯 on 15/11/30.
+//  Copyright © 2015年 uYoungCommon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateActivityStep2Controller : UIViewController
+
+@end
---
+++ uYoung/uYoung/CreateActivityStep2Controller.m
@@ -0,0 +1,37 @@
+//
+//  CreateActivityStep2Controller.m
+//  uYoung
+//
+//  Created by 独自天涯 on 15/11/30.
+//  Copyright © 2015年 uYoungCommon. All rights reserved.
+//
+
+#import "CreateActivityStep2Controller.h"
+
+@interface CreateActivityStep2Controller ()
+
+@end
+
+@implementation CreateActivityStep2Controller
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
